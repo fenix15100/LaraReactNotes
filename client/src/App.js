e2bf21c56b45ae447eb9ea1dcd66a567a7002f14
Francisco José Laptop <francisco.camacho15@gmail.com>
2019-04-29 15:24:58 +0200

    Add support to login with JWT client react

--- client/src/App.js
+++ client/src/App.js
@@ -2,27 +2,30 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import AuthService from './Components/AuthServices';
+import withAuth from './Components/withAuth';
+const Auth = new AuthService();
 
-export default App;
+class App extends Component {
+    handleLogout() {
+        Auth.logout();
+        this.props.history.replace('/login');
+    }
+
+    render() {
+
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome {this.props.user.username}</h2>
+                </div>
+                <p className="App-intro">
+                    <button type="button" className="form-submit" onClick={this.handleLogout.bind(this)}>Logout</button>
+                </p>
+            </div>
+        );
+
+    }
+}
+export default withAuth(App);
